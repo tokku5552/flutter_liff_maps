879cc1d5b3c160d72427d7ecafe3f530008c7bdb
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-06-29 23:11:56 +0900

    feat: enabe to check in and show radius slider

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        sliderTheme: SliderThemeData(
+          overlayShape: SliderComponentShape.noOverlay,
+        ),
       ),
       scrollBehavior: _AppScrollBehavior(),
       debugShowCheckedModeBanner: false,
