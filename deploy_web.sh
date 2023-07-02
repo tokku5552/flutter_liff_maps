683943a79095a49599e2fe48d89f459ba6f9860f
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-07-02 13:53:53 +0900

    chore: update deploy command

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,1 +1,1 @@
-fvm flutter build web --dart-define-from-file=dart_define.json && firebase deploy --only hosting
+fvm flutter build web --dart-define-from-file=dart_define.json && firebase use default && firebase deploy --only hosting
